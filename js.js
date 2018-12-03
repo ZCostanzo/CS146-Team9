6023c56daaa88e764d7507f2d42e6371b7f1eeb0
coriwisniewski <43626560+coriwisniewski@users.noreply.github.com>
2018-12-03 18:25:49 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -24,3 +24,10 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+function printAlert(){
+		alert("Thank you for your feedback! We will get back to you shortly");
+}
+function redirect() {
+	window.open('Form.html', '_blank');
+}
